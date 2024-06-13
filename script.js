58f2e7a135d4b040722556c0a70d9ba38d98b25b
Oladimeji402 <olamoney665@gmail.com>
2024-06-13 18:57:43 +0100

    hojkgyh

---
+++ script.js
@@ -0,0 +1,10 @@
+function letterFinder(word, match, i){
+    if (word[i] === match){
+      console.log('word', match, 'at', i){
+      }
+    }
+    else{
+      console.log("",i),
+    }
+  }
+  letterFinder("test","t")
